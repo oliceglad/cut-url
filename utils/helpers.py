5612a1475550f18a37172d9b2398a1d3c78df00f
oliceglad <vycheslavofficial@gmail.com>
2024-05-29 22:05:11 +0400

    deleted split methods

--- utils/helpers.py
+++ utils/helpers.py
@@ -3,23 +3,39 @@ import string
 from common import messages
 from utils import regex, storage, url_request
 from logger.logger_config import logger
+def _split(text_):
+    word = []
+    t = []
+    if "https" in text_:
+        text_ = text_[5:]
+    else:
+        text_ = text_[4:]
+    for i in text_:
+        if i == '.':
+            word.append(''.join(t))
+            t = []
+        else:
+            t.append(i)
+    if t:
+        word.append(''.join(t))
+    return word
 
 def generate_short_url(full_url):
-    domain_and_zone = full_url.split('//')[-1].split('/')[0]
-    domain, zone = domain_and_zone.split('.')
+    print(full_url, _split(full_url))
+    domain, zone = _split(full_url)[0], _split(full_url)[1]
     consonants_only = regex.extract_consonants(domain)
     short_url = f"{consonants_only}.{zone}"
     return short_url
 
 
 def register_url():
-    full_url = input(messages.REGISTER_PROMPT).strip().lower()
+    full_url = input(messages.REGISTER_PROMPT).lower()
     if not regex.is_valid_url(full_url):
         logger.error("Invalid URL format")
         print("Неверный формат URL")
         return
 
-    alias = full_url.split('/')[2]
+    alias = full_url
     short_url = generate_short_url(alias)
     short_url = short_url
     storage.add_url_mapping(short_url, full_url, alias)
